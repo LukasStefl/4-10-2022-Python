9722e92f066db555ec4c76c1c8ad19a40f55e148
LukasStefl <lukas.stefl@student.ossp.cz>
2022-10-04 09:53:17 +0200

    commit

---
+++ Terti_ukol.py
@@ -0,0 +1,9 @@
+def reverse_string(str):  
+    str1 = "" 
+    for i in str:  
+        str1 = i + str1  
+    return str1 
+     
+str = "JavaTpoint"    
+print(str)  
+print(reverse_string(str)) 
